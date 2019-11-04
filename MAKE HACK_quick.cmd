0249849c98aa08d5ec5014438ffe8ad4ee37d944
jfarr095 <jfarr095@uottawa.ca>
2019-11-03 20:28:36 -0500

    First

---
+++ MAKE HACK_quick.cmd
@@ -0,0 +1,13 @@
+﻿cd %~dp0
+
+copy YggdraHack.gba YggdraHack_EngTrans.gba
+
+cd "%~dp0Event Assembler"
+
+Core A FE8 "-output:%~dp0YggdraHack_EngTrans.gba" "-input:%~dp0ROM Buildfile.event"
+
+cd ".."
+
+start /w FEBuilder/FEBuilderGBA.exe --translate --fromrom=YggdraHack_EngTrans.gba --torom=FE8_Clean.gba --importfont YggdraHack_EngTrans.gba
+
+pause
---
+++ MAKE HACK_text.cmd
@@ -0,0 +1,17 @@
+﻿cd %~dp0
+
+copy YggdraHack.gba YggdraHack_EngTrans.gba
+
+cd "%~dp0Text"
+
+echo: | (text-process-classic text_buildfile.txt --parser-exe "%~dp0Event Assembler/Tools/ParseFile.exe")
+
+cd "%~dp0Event Assembler"
+
+Core A FE8 "-output:%~dp0YggdraHack_EngTrans.gba" "-input:%~dp0ROM Buildfile.event"
+
+cd ".."
+
+start /w FEBuilder/FEBuilderGBA.exe --translate --fromrom=YggdraHack_EngTrans.gba --torom=FE8_Clean.gba --importfont YggdraHack_EngTrans.gba
+
+pause
